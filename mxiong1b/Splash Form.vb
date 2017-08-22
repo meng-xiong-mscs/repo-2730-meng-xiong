f14bd8fe0001ba66528f74ca9cb1519eca07fe4d
Meng Xiong <Meng Xiong>
2017-08-22 13:05:35 -0500

    Add project files.

---
+++ mxiong1b/Splash Form.vb
@@ -0,0 +1,13 @@
+﻿Public Class frmSplash
+    Private Sub picStorks_Click(sender As Object, e As EventArgs) Handles picStorks.Click
+
+    End Sub
+
+    Private Sub frmSplash_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub tmrExit_Tick(sender As Object, e As EventArgs) Handles tmrExit.Tick
+        Me.Close()
+    End Sub
+End Class
