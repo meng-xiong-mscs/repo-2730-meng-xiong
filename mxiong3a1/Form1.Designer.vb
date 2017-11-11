7dbec5aafb415fa5f8425e5db93a18cc1c152b95
mengx <mengx@LAPTOP-UKDIQ5O7>
2017-11-11 14:41:42 -0600

    Added unfinished 3a1 for backup

---
+++ mxiong3a1/Form1.Designer.vb
@@ -0,0 +1,195 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.pretestDoWhilebtn = New System.Windows.Forms.Button()
+        Me.pretestDoUntilbtn = New System.Windows.Forms.Button()
+        Me.posttestLoopWhilebtn = New System.Windows.Forms.Button()
+        Me.posttestLoopUntilbtn = New System.Windows.Forms.Button()
+        Me.forLoopbtn = New System.Windows.Forms.Button()
+        Me.lblEven = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblEven)
+        Me.GroupBox1.Controls.Add(Me.forLoopbtn)
+        Me.GroupBox1.Controls.Add(Me.posttestLoopUntilbtn)
+        Me.GroupBox1.Controls.Add(Me.posttestLoopWhilebtn)
+        Me.GroupBox1.Controls.Add(Me.pretestDoUntilbtn)
+        Me.GroupBox1.Controls.Add(Me.pretestDoWhilebtn)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(453, 315)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "p347 #13 loop 2-20"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label2)
+        Me.GroupBox2.Controls.Add(Me.Label1)
+        Me.GroupBox2.Controls.Add(Me.Button3)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Controls.Add(Me.Button1)
+        Me.GroupBox2.Location = New System.Drawing.Point(507, 12)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(350, 298)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "p332-p348 #22 Addition"
+        '
+        'pretestDoWhilebtn
+        '
+        Me.pretestDoWhilebtn.Location = New System.Drawing.Point(228, 39)
+        Me.pretestDoWhilebtn.Name = "pretestDoWhilebtn"
+        Me.pretestDoWhilebtn.Size = New System.Drawing.Size(138, 35)
+        Me.pretestDoWhilebtn.TabIndex = 0
+        Me.pretestDoWhilebtn.Text = "Pretest: Do While"
+        Me.pretestDoWhilebtn.UseVisualStyleBackColor = True
+        '
+        'pretestDoUntilbtn
+        '
+        Me.pretestDoUntilbtn.Location = New System.Drawing.Point(228, 80)
+        Me.pretestDoUntilbtn.Name = "pretestDoUntilbtn"
+        Me.pretestDoUntilbtn.Size = New System.Drawing.Size(138, 35)
+        Me.pretestDoUntilbtn.TabIndex = 1
+        Me.pretestDoUntilbtn.Text = "Pretest: Do Until"
+        Me.pretestDoUntilbtn.UseVisualStyleBackColor = True
+        '
+        'posttestLoopWhilebtn
+        '
+        Me.posttestLoopWhilebtn.Location = New System.Drawing.Point(228, 126)
+        Me.posttestLoopWhilebtn.Name = "posttestLoopWhilebtn"
+        Me.posttestLoopWhilebtn.Size = New System.Drawing.Size(138, 35)
+        Me.posttestLoopWhilebtn.TabIndex = 2
+        Me.posttestLoopWhilebtn.Text = "Posttest: Loop While"
+        Me.posttestLoopWhilebtn.UseVisualStyleBackColor = True
+        '
+        'posttestLoopUntilbtn
+        '
+        Me.posttestLoopUntilbtn.Location = New System.Drawing.Point(228, 166)
+        Me.posttestLoopUntilbtn.Name = "posttestLoopUntilbtn"
+        Me.posttestLoopUntilbtn.Size = New System.Drawing.Size(138, 35)
+        Me.posttestLoopUntilbtn.TabIndex = 3
+        Me.posttestLoopUntilbtn.Text = "Posttest: Loop Until"
+        Me.posttestLoopUntilbtn.UseVisualStyleBackColor = True
+        '
+        'forLoopbtn
+        '
+        Me.forLoopbtn.Location = New System.Drawing.Point(228, 204)
+        Me.forLoopbtn.Name = "forLoopbtn"
+        Me.forLoopbtn.Size = New System.Drawing.Size(138, 35)
+        Me.forLoopbtn.TabIndex = 4
+        Me.forLoopbtn.Text = "Pretest: For"
+        Me.forLoopbtn.UseVisualStyleBackColor = True
+        '
+        'lblEven
+        '
+        Me.lblEven.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblEven.Location = New System.Drawing.Point(17, 39)
+        Me.lblEven.Name = "lblEven"
+        Me.lblEven.Size = New System.Drawing.Size(196, 259)
+        Me.lblEven.TabIndex = 5
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(206, 107)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(138, 35)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Sum, Do While"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(206, 166)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(138, 35)
+        Me.Button2.TabIndex = 2
+        Me.Button2.Text = "Sum, Loop Until"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(206, 222)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(138, 35)
+        Me.Button3.TabIndex = 3
+        Me.Button3.Text = "Sum, For Next"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label1.Location = New System.Drawing.Point(206, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(59, 38)
+        Me.Label1.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label2.Location = New System.Drawing.Point(285, 56)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(59, 38)
+        Me.Label2.TabIndex = 5
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(920, 560)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "mxiong3a1 loops, sum, count"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents forLoopbtn As Button
+    Friend WithEvents posttestLoopUntilbtn As Button
+    Friend WithEvents posttestLoopWhilebtn As Button
+    Friend WithEvents pretestDoUntilbtn As Button
+    Friend WithEvents pretestDoWhilebtn As Button
+    Friend WithEvents lblEven As Label
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ mxiong3a1/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+    Private Sub pretestDoWhilebtn_Click(sender As Object, e As EventArgs) Handles pretestDoWhilebtn.Click
+        lblEven.Text = "Pretest Do While" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do While i <= 20
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop
+    End Sub
+
+    Private Sub pretestDoUntilbtn_Click(sender As Object, e As EventArgs) Handles pretestDoUntilbtn.Click
+        lblEven.Text = "Pretest Do Until" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do Until i = 22
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop
+    End Sub
+
+    Private Sub posttestLoopWhilebtn_Click(sender As Object, e As EventArgs) Handles posttestLoopWhilebtn.Click
+        lblEven.Text = "PostTest Loop While" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop While i <= 20
+    End Sub
+
+    Private Sub posttestLoopUntilbtn_Click(sender As Object, e As EventArgs) Handles posttestLoopUntilbtn.Click
+        lblEven.Text = "PostTest Loop Until" & ControlChars.NewLine
+        Dim i As Integer = 2
+        Do
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 2
+        Loop Until i = 22
+    End Sub
+
+    Private Sub forLoopbtn_Click(sender As Object, e As EventArgs) Handles forLoopbtn.Click
+        lblEven.Text = "Pretest For" & ControlChars.NewLine
+        For i As Integer = 2 To 20
+            lblEven.Text = lblEven.Text & i.ToString & ControlChars.NewLine
+            i += 1
+        Next i
+
+    End Sub
+End Class
