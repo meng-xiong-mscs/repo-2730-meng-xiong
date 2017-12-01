6484e2fed51d0755b62f83cfec65ce65e46d6695
mengx <mengx@LAPTOP-UKDIQ5O7>
2017-12-01 16:41:42 -0600

    3d1 question

---
+++ mxiong3d1/Form1.Designer.vb
@@ -0,0 +1,238 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.txtMonth = New System.Windows.Forms.TextBox()
+        Me.lblDays = New System.Windows.Forms.Label()
+        Me.btnDisplayDays = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnDoLoop = New System.Windows.Forms.Button()
+        Me.btnForEachNext = New System.Windows.Forms.Button()
+        Me.btnForNext = New System.Windows.Forms.Button()
+        Me.lblAvg = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lblLessThanAvg = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.btnDisplayDays)
+        Me.GroupBox1.Controls.Add(Me.lblDays)
+        Me.GroupBox1.Controls.Add(Me.txtMonth)
+        Me.GroupBox1.Location = New System.Drawing.Point(13, 26)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(268, 233)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Days in month"
+        '
+        'txtMonth
+        '
+        Me.txtMonth.Location = New System.Drawing.Point(154, 50)
+        Me.txtMonth.Name = "txtMonth"
+        Me.txtMonth.Size = New System.Drawing.Size(65, 22)
+        Me.txtMonth.TabIndex = 7
+        Me.txtMonth.Text = "1"
+        '
+        'lblDays
+        '
+        Me.lblDays.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblDays.Location = New System.Drawing.Point(154, 94)
+        Me.lblDays.Name = "lblDays"
+        Me.lblDays.Size = New System.Drawing.Size(65, 28)
+        Me.lblDays.TabIndex = 8
+        '
+        'btnDisplayDays
+        '
+        Me.btnDisplayDays.Location = New System.Drawing.Point(71, 169)
+        Me.btnDisplayDays.Name = "btnDisplayDays"
+        Me.btnDisplayDays.Size = New System.Drawing.Size(115, 40)
+        Me.btnDisplayDays.TabIndex = 9
+        Me.btnDisplayDays.Text = "&Display Days"
+        Me.btnDisplayDays.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(33, 50)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 17)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "&Month Number:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(33, 105)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 17)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "&Number of Days:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label6)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.lblLessThanAvg)
+        Me.GroupBox2.Controls.Add(Me.btnExit)
+        Me.GroupBox2.Controls.Add(Me.btnDoLoop)
+        Me.GroupBox2.Controls.Add(Me.btnForEachNext)
+        Me.GroupBox2.Controls.Add(Me.btnForNext)
+        Me.GroupBox2.Controls.Add(Me.lblAvg)
+        Me.GroupBox2.Controls.Add(Me.Label3)
+        Me.GroupBox2.Location = New System.Drawing.Point(329, 26)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(469, 233)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "P522 #9 Avg, min sold"
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(283, 147)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(111, 28)
+        Me.btnExit.TabIndex = 9
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnDoLoop
+        '
+        Me.btnDoLoop.Location = New System.Drawing.Point(283, 117)
+        Me.btnDoLoop.Name = "btnDoLoop"
+        Me.btnDoLoop.Size = New System.Drawing.Size(111, 28)
+        Me.btnDoLoop.TabIndex = 8
+        Me.btnDoLoop.Text = "&Do...Loop"
+        Me.btnDoLoop.UseVisualStyleBackColor = True
+        '
+        'btnForEachNext
+        '
+        Me.btnForEachNext.Location = New System.Drawing.Point(283, 87)
+        Me.btnForEachNext.Name = "btnForEachNext"
+        Me.btnForEachNext.Size = New System.Drawing.Size(111, 28)
+        Me.btnForEachNext.TabIndex = 7
+        Me.btnForEachNext.Text = "For &Each...Next"
+        Me.btnForEachNext.UseVisualStyleBackColor = True
+        '
+        'btnForNext
+        '
+        Me.btnForNext.Location = New System.Drawing.Point(283, 57)
+        Me.btnForNext.Name = "btnForNext"
+        Me.btnForNext.Size = New System.Drawing.Size(111, 28)
+        Me.btnForNext.TabIndex = 6
+        Me.btnForNext.Text = "&For...Next"
+        Me.btnForNext.UseVisualStyleBackColor = True
+        '
+        'lblAvg
+        '
+        Me.lblAvg.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblAvg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblAvg.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblAvg.Location = New System.Drawing.Point(99, 76)
+        Me.lblAvg.Name = "lblAvg"
+        Me.lblAvg.Size = New System.Drawing.Size(133, 46)
+        Me.lblAvg.TabIndex = 11
+        Me.lblAvg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(6, 87)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(68, 17)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Avg Sold:"
+        '
+        'lblLessThanAvg
+        '
+        Me.lblLessThanAvg.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.lblLessThanAvg.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.lblLessThanAvg.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblLessThanAvg.Location = New System.Drawing.Point(99, 147)
+        Me.lblLessThanAvg.Name = "lblLessThanAvg"
+        Me.lblLessThanAvg.Size = New System.Drawing.Size(133, 46)
+        Me.lblLessThanAvg.TabIndex = 12
+        Me.lblLessThanAvg.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(15, 158)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 17)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "< Avg:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(15, 36)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(179, 17)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Values: 250, 225, 193, 260"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(908, 549)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "mxiong3d1 Arrays, subscripts. loops"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents lblDays As Label
+    Friend WithEvents txtMonth As TextBox
+    Friend WithEvents btnDisplayDays As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnDoLoop As Button
+    Friend WithEvents btnForEachNext As Button
+    Friend WithEvents btnForNext As Button
+    Friend WithEvents lblAvg As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents lblLessThanAvg As Label
+    Friend WithEvents Label6 As Label
+End Class
---
+++ mxiong3d1/Form1.vb
@@ -0,0 +1,46 @@
+﻿'Name:          Exercise 3D: Array subscripts, totals, avg, min
+'Purpose:       Displays the average number sold
+'Programmer:    Meng Xiong on 12/1/2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class Form1
+    Private intsold() As Integer = {250, 225, 192, 260}
+    Private dblWholeSale() As Double = {17.5, 9, 10.5, 18.75, 26, 8.5, 5.65, 10, 30}
+
+    Private Sub btnDisplayDays_Click(sender As Object, e As EventArgs) Handles btnDisplayDays.Click
+
+        Static intNum(11) As Integer
+        Dim intMonthNumber As Integer
+        Dim intDaysInMonth() As Integer = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31}
+        If Integer.TryParse(txtMonth.Text, intMonthNumber) AndAlso intMonthNumber >= 1 AndAlso intMonthNumber <= 12 Then
+            lblDays.Text = intDaysInMonth(intMonthNumber).ToString
+        Else
+            MessageBox.Show("Invalid month. Enter a number from 1 to 12.", "Month", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+        txtMonth.Focus()
+        txtMonth.SelectAll()
+    End Sub
+    Private Sub btnForNext_Click(sender As Object, e As EventArgs) Handles btnForNext.Click
+        Dim dblavg As Double
+        Dim inttotal As Integer = intsold.GetUpperBound(0)
+        For intnum As Integer = 0 To inttotal
+            dblavg = intnum + intsold(inttotal)
+        Next intnum
+        lblAvg.Text = dblavg.ToString
+    End Sub
+
+    Private Sub btnForEachNext_Click(sender As Object, e As EventArgs) Handles btnForEachNext.Click
+
+    End Sub
+
+    Private Sub btnDoLoop_Click(sender As Object, e As EventArgs) Handles btnDoLoop.Click
+
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
